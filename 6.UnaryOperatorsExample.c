8fbf6f8295139c324405a7dec3ed90087789a6f9
Lynn-code-lab <64342965+Lynn-code-lab@users.noreply.github.com>
2020-04-28 17:14:44 +0630

    Add files via upload

---
+++ 6.UnaryOperatorsExample.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+/**********************************************************
+Author : Lynn Myat Thaw
+Purpose : To learn about unary operators
+**********************************************************/
+void main ()
+{
+    int a=5;
+    int b;
+    b=a--;
+    printf("a=%d , b=%d",a,b);
+    getch ();
+}
